f4857bb3e6738cabac81cf91105a981280b580a8
Sid Ravinutala <sid.ravi1@gmail.com>
2021-08-13 11:09:34 -0400

    ch12 added

---
+++ ch12/ch12_problem.py
@@ -0,0 +1,91 @@
+## Q1: Here are a bunch of classes. Refactor them taking into account the 8 principles
+##     under "Coping with Multiple Inheritance" on page 362. You are welcome to change
+##     anything you like and add additional classes etc. - I'm hoping this
+##     makes for a healty discussion on inheritance and design choices.
+
+
+class LivingThing:
+    def __init__(self, name):
+        self.name = name
+
+    def birth(self):
+        print("f{self} is born")
+
+    def death(self):
+        print("f{self} is dead")
+
+
+class ThingsThatMove:
+    def move_left(self):
+        print(f"{self} moves left")
+
+    def move_right(self):
+        print(f"{self} moves right")
+
+    def move_forward(self):
+        print(f"{self} moves forward")
+
+    def move_backward(self):
+        print(f"{self} moves backward")
+
+
+class ThingsThatFly(ThingsThatMove):
+    def move_up(self):
+        print(f"{self} moves up")
+
+    def move_down(self):
+        print(f"{self} moves up")
+
+
+class ThingsThatBirthYoung:
+    def birth(self):
+        print("f{self} birthed a baby")
+
+    def suckle_young(self):
+        print("f{suckle}")
+
+
+class ThingsThatLayEggs:
+    def birth(self):
+        print(f"{self} laid an egg")
+
+
+class Mammal(LivingThing, ThingsThatMove):
+    def __repr__(self):
+        return f"Animal[{self.name}]"
+
+
+class Bird(LivingThing, ThingsThatFly, ThingsThatLayEggs):
+    def __repr__(self):
+        return f"Bird[{self.name}]"
+
+
+## Q2: create the following subclasses
+### Q2.1
+class Peacock:
+    # Shoukd also dance in the rain
+    pass
+
+
+### Q2.2
+class Bat:
+    # It's a weird mammal
+    pass
+
+
+### Q2.3
+class Platypus:
+    # Lol. Good luck.
+    pass
+
+
+### Q2.4
+class Whale:
+    # Can move in all directions in water
+    pass
+
+
+### Q2.5
+class Human:
+    # Should also pay taxes
+    pass
