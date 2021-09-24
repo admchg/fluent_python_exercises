5fa1222f95bdf9ff39f37a1713691d628a3c9e29
Tanmay Verma <tanmay.verma@idinsight.org>
2021-09-24 13:27:56 +0530

    ch16 problem

---
+++ ch16/ch16_problem.py
@@ -0,0 +1,50 @@
+# Ch 16 Couroutines
+# THE PESTERING BUTLER
+# Q. With the knowledge of coroutines, we now know that you can have 'conversations' with a function
+# i.e. you can send and receive information. Use this principle to create a butler 'chatbot' that
+# cyclically offers you 3 beverage choices (latte, espresso, capuccino), and accepts your input.
+# When your input is 1, the butler brings you the beverage; when your input is 99, the butler leaves. 
+# For all other inputs, the butler keeps pestering you about which beverage you'd like.
+
+## Sample Conversation (The numbers are inputs by the user)
+# <The program is launched>
+# You want latte?
+# 0
+# You want espresso?
+# 3
+# You want capuccino?
+# 05
+# You want latte?
+# 2
+# You want espresso?
+# 1
+# Fine, I will get you espresso
+
+
+## Sample Conversation 2 (The numbers are inputs by the user)
+# <The program is launched>
+# You want latte?
+# 5
+# You want espresso?
+# 99
+# Okay I will leave. But please don't fire me, Sir.
+# <The program exits here>
+
+
+from itertools import cycle  
+def beverage():
+    # Initialize and yield a choice of beverages cyclically
+    # Break away if coroutine is 'sent' a '1'
+    pass
+
+def serve():
+    # Initialize and prime the beverage couroutine
+    # Start taking user inputs
+    # You can set 99 as the escape (break) request and send other responses to the coroutine.
+    # Finally, catch exception to accept the order.
+    pass
+        
+        
+if __name__ == "__main__":
+    serve()
+
