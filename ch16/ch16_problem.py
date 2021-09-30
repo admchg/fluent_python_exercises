2ce29058f3e2627b805a39ba6abd5b0e2feacb17
Sid Ravinutala <sid.ravi1@gmail.com>
2021-09-29 22:24:16 -0400

    ch16 soln

--- ch16/ch16_problem.py
+++ ch16/ch16_problem.py
@@ -3,7 +3,7 @@
 # Q. With the knowledge of coroutines, we now know that you can have 'conversations' with a function
 # i.e. you can send and receive information. Use this principle to create a butler 'chatbot' that
 # cyclically offers you 3 beverage choices (latte, espresso, capuccino), and accepts your input.
-# When your input is 1, the butler brings you the beverage; when your input is 99, the butler leaves. 
+# When your input is 1, the butler brings you the beverage; when your input is 99, the butler leaves.
 # For all other inputs, the butler keeps pestering you about which beverage you'd like.
 
 ## Sample Conversation (The numbers are inputs by the user)
@@ -32,20 +32,43 @@
 # <The program exits here>
 
 
-from itertools import cycle  
+from itertools import cycle
+
+
 def beverage():
+    beverages = cycle(["espresso", "capuccino", "latte"])
+
+    request = yield None
+    while True:
+        bev = next(beverages)
+        request = yield f"You want {bev}"
+        if request == "1":
+            break
+
     # Initialize and yield a choice of beverages cyclically
     # Break away if coroutine is 'sent' a '1'
-    pass
+    return f"Fine, I will get you a {bev}"
+
 
 def serve():
     # Initialize and prime the beverage couroutine
     # Start taking user inputs
     # You can set 99 as the escape (break) request and send other responses to the coroutine.
     # Finally, catch exception to accept the order.
-    pass
-        
-        
+    bev = beverage()
+    next(bev)
+    print(bev.send(0))
+    while True:
+        req = input()
+        if req == "99":
+            return "Okay I will leave. But please don't fire me, Sir."
+        else:
+            try:
+                print(bev.send(req))
+            except StopIteration as exc:
+                print(exc.value)
+                break
+
+
 if __name__ == "__main__":
     serve()
-
